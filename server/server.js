462d0659ed734bda0e75416ce8561a2678852c6a
Alexandria Ellis <allstarcoding777@outlook.com>
2023-05-11 14:28:40 -0500

    incorporated mongoose

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
-// require express package
-const express = require('express')
-
-// attaches environment variables
+// require('dotenv').config() to access environment variables
 require('dotenv').config()
 
+// require express package and mongoose package in order to use them
+const express = require('express')
+const mongoose = require('mongoose')
+
+// require workout routes from routes folder
 const workoutRoutes = require('./routes/Workouts')
 
-// starts express app, function that creates express app
+// create instance of express application
 const app = express()
 
-// middleware, logs requests that come in
+// middleware to parse incoming data
 app.use(express.json())
 
+// middleware to log requests
 app.use((req, res, next) => {
 console.log (req.path, req.method)
 next()
@@ -20,7 +23,15 @@ next()
 // attaches all workout routes to application
 app.use('/api/workouts', workoutRoutes)
 
-// listen for requests on PORT
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port', process.env.PORT)
-})
+// connect to database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+    // listen for requests on PORT
+    app.listen(process.env.PORT, () => {
+      console.log('Connected to database & listening on port', process.env.PORT)
+    })  
+ })
+   // if there is an error, log it
+   .catch((error) => {
+     console.log(error)
+ })
