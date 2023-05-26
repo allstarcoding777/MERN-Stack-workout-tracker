d2c5db60be32a2e86077d6552a1e487b8a864a48
Alexandria Ellis <allstarcoding777@outlook.com>
2023-05-26 11:22:41 -0500

    login & signup authentication

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,14 +1,48 @@
+// require the User model
 const User= require('../models/UserModel')
+// require jsonwebtoken
+const jwt = require('jsonwebtoken')
+
+// generate token function - this function will be used to create tokens for users
+const generateToken = (_id) => {
+    // return jwt.sign(payload, secretOrPrivateKey, [options, callback])
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '2d'})
+}
 
 // login user
 const userLogin = async (req, res) => {
-res.json({ message: 'login user' })
+    const {email, password} = req.body
+
+    try {
+        // login user with email and password
+        const user = await User.login(email, password)
+        // create token
+        const token = generateToken(user._id)
+        // send token and email to client
+        res.status(200).json({email, token})
+    // if there is an error, send error message to the client
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 // signup user
 const userSignup = async (req, res) => {
-    res.json({ message: 'signup user' })
+    // get email and password from req.body
+    const {email, password} = req.body
+
+    try {
+        // create user with email and password
+        const user = await User.signup(email, password)
+        // create token
+        const token = generateToken(user._id)
+        // send token and email to client
+        res.status(200).json({email, token})
+    // if there is an error, send error message to the client
+    } catch (error) {
+        res.status(400).json({error: error.message})
     }
+}
 
 // export the functions
 module.exports = { userLogin, userSignup }
--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,7 +1,14 @@
+// require mongoose, mongoose is an ODM (Object Document Mapper) that allows us to interact with MongoDB
 const mongoose = require('mongoose')
+// require bcrypt, bcrypt is a library that allows us to hash passwords
+const bcrypt = require('bcrypt')
+// require validator, validator is a library that allows us to validate data
+const validator = require('validator')
 
+// create a schema, a schema is a blueprint for our data
 const Schema = mongoose.Schema
 
+// create a user schema with email and password fields
 const userSchema = new Schema({
     email:{
         type: String,
@@ -14,5 +21,61 @@ const userSchema = new Schema({
     }
 })
 
+// static signup method
+userSchema.statics.signup = async function(email, password) {
+    
+    //if email or password is missing, throw error
+    if (!email || !password) {
+        throw Error('all fields required')
+    }
+    // if email is invalid, throw error
+    if (!validator.isEmail(email)) {
+        throw Error('invalid email')
+    }
+    // if password is not strong enough, throw error
+    if(!validator.isStrongPassword(password)) {
+        throw Error('password not strong enough')
+    }
+    // check if email is in use
+    const exists = await this.findOne({ email })
+    // if not, throw error
+    if (exists) {
+        throw Error('email already in use')
+    }
+    // salt password, which means to add random characters to the password
+    const salt = await bcrypt.genSalt(12)
+    // hash password, hashing means to encrypt the password
+    const hash = await bcrypt.hash(password, salt)
+    // create user
+    const user = await this.create({ email, password: hash})
+
+    return user
+
+}
+
+// static login method
+userSchema.statics.login = async function(email, password) {
+    // if email or password is missing, throw error
+    if (!email || !password) {
+        throw Error('all fields required')
+    }
+    // if email is invalid, throw error
+    const user = await this.findOne({ email})
+    if (!user) {
+        throw Error('invalid email')
+    }
+
+    // compare password with hashed password
+    const mathches = await bcrypt.compare(password, user.password)
+
+    // if password does not match, throw error
+    if (!mathches) {
+        throw Error('invalid password')
+    }
+
+    return user
+}
+
+// mongoose.model is a function that takes two arguments - the name of the model and the schema
 module.exports = mongoose.model('User', userSchema)
 
